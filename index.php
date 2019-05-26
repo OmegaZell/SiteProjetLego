9d1fb983bece64d583363aa942ca6ee1f9c98f72
OmegaZell <47260533+OmegaZell@users.noreply.github.com>
2019-05-26 16:03:32 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 ﻿<?php
-	include_once("pt2.html");
-	include_once("equipe.html");
-	include_once("style.css");
-	include_once("journal.html");
-	include_once("programmes.html");
+	include_once ( " pt2.html " );
+	include_once ( " equipe.html " );
+	include_once ( " style.css " );
+	include_once ( " journal.html " );
+	include_once ( " programmes.html " );
 ?>
