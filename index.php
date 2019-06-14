7efa2d17113dc26ade45a855e396639000216b5f
OmegaZell <47260533+OmegaZell@users.noreply.github.com>
2019-06-14 13:04:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 	include_once ( " home.html " );
 	include_once ( " equipe.html " );
 	include_once ( " style.css " );
-	include_once ( " journal.html " );
+	include_once ( " medias.html " );
 	include_once ( " programmes.html " );
-	include_once ( " bannière.jpg " );
 	include_once ( " iut.png " );
 	include_once ( " piege.png " );
 	include_once ( " robot.png " );
 	include_once ( " laby.png " );
-
+	include_once ( " first.png " );
+	include_once ( " geii.png " );
 ?>
