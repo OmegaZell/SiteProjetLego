8fd9cd3bf4b3feda69278272350a8330ddfc20a1
OmegaZell <47260533+OmegaZell@users.noreply.github.com>
2019-06-21 13:18:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+	include_once("home.html");
+	include_once("equipe.html");
+	include_once("medias.html");
+	include_once("programmes.html");
+	include_once("debut.png");
+	include_once("esquisse.png");
+	include_once("fini.png");
+	include_once("formuleun.jpg");
+	include_once("geii.png");
+	include_once("iut.png");
+	include_once("laby.png");
+	include_once("piege.png");
+	include_once("programmedeux.jpg");
+	include_once("runcircuit.mp4");
+	include_once("runlaby.mp4");
+	include_once("style.css");
+	?>
